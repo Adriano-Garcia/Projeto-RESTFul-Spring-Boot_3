a336163a2f951bab644116d7f7d88066bef77ad6
Adriano Garcia <adrianogarcia02@gmail.com>
2023-06-11 16:44:30 -0400

    first commit

---
+++ src/main/java/dominando/restful/ProjectRestfulApplication.java
@@ -0,0 +1,13 @@
+package dominando.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectRestfulApplication.class, args);
+	}
+
+}
---
+++ src/main/java/dominando/restful/services/UserService.java
@@ -0,0 +1,8 @@
+package dominando.restful.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
